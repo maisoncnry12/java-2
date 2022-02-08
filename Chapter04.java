8037646ee32426e4acc73c8473aa4f53935c6334
maisoncnry12 <caxxry@gmail.com>
2022-02-08 11:41:16 +0900

    [Add]Variable01,Chapter04

---
+++ Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 2;
+		String str = "Kanari Matsumoto";
+		System.out.println(str);
+		str = "Thanks!";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
