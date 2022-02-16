2e55d3801f9058fbf64852ae6de525ca57ae2458
maisoncnry12 <caxxry@gmail.com>
2022-02-16 10:07:49 +0900

    [Add]if文

---
+++ Chapter07.java
@@ -0,0 +1,15 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		int distance = 15;
+		if (distance <= 5) {
+			System.out.println("わりと近い方！");
+		} else if (distance <= 7) {
+			System.out.println("少し遠い！！");
+		} else if (distance <= 10) {
+			System.out.println("めっちゃ遠い！！！");
+		} else {
+			System.out.println("覚悟がいります。");
+		}
+	}
+}
