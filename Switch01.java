ec5797868776a3c8c01de7af06a393d2bba85e67
maisoncnry12 <caxxry@gmail.com>
2022-02-16 09:44:23 +0900

    [Add]switch文

---
+++ Switch01.java
@@ -0,0 +1,29 @@
+
+public class Switch01 {
+	public static void main(String[] args) {
+		int val = 2;
+		switch (val) {
+		case 3:
+		    System.out.println("よくできました！");
+		    break;
+		case 2:
+			System.out.println("ふつうでした！");
+			break;
+		default:
+			System.out.println("がんばろう！");
+		}
+	}
+}
+
+//＜switch文の基本構文＞ーーーーーーー
+//switch (式（変数や値）){
+//case 値:
+//    ブロック(処理);
+//    break;
+//case 値:
+//    ブロック(処理);
+//    break;
+//default:
+//    ブロック(処理);
+//}
+//ーーーーーーーーーーーーーーーーーーー
