5a82fa9a2b352b4bd8edb714155f7b4eb99f7a96
maisoncnry12 <caxxry@gmail.com>
2022-02-17 09:52:42 +0900

    [Add]for文

---
+++ For01.java
@@ -0,0 +1,14 @@
+
+public class For01 {
+	public static void main (String[] args) {
+		for (int number = 1; number <= 5; number++) {
+			System.out.println("for01 = " + number);
+		}
+	}
+}
+
+//＜for文の基本構文＞-------------
+//for (初期化式; 条件式; 変化式) {
+//    ブロック(繰り返し処理)
+//}
+//------------------------------
