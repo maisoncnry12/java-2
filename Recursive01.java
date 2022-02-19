f30855ba33dcb861b4a645c56922e3362de6afce
maisoncnry12 <caxxry@gmail.com>
2022-02-19 11:55:45 +0900

    [Add]再帰処理

--- Recursive01.java
+++ Recursive01.java
@@ -1,4 +1,20 @@
 
 public class Recursive01 {
+	public static void main(String[] args) {
+		int number = 1;
+		printNum(number);
+	}
 
+	public static int printNum(int number) {
+		if (number < 50) {
+			number *= 2;
+			System.out.println("While01 = " + number);
+			// 条件にマッチしたら自分自身を再度呼び出す
+			printNum(number);
+		}
+		return number;
+	}
 }
+
+//＜再帰処理＞
+//あるメソッドが自分自身を呼び出すことで繰り返しを実現することができる。
