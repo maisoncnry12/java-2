f2d798c3e0f126da5cb813bc835c27d09adfff89
maisoncnry12 <caxxry@gmail.com>
2022-02-21 10:22:32 +0900

    [Add]while文,for文

--- Chapter08.java
+++ Chapter08.java
@@ -7,8 +7,18 @@ public class Chapter08 {
 			System.out.println(num * num);
 			num++;
 		}
-		
-		
+		// 2.通常for文
+		int array[] = { 1, 4, 14, 25 };
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		// 3.拡張for文
+		for (int val : array) {
+			if (val % 2 == 0) {
+				continue;
+			}
+			System.out.println(val);
+		}
 	}
 }
 
