5b7918445e7bacd590b18de782951dfc20c5a9fa
maisoncnry12 <caxxry@gmail.com>
2022-03-28 13:46:37 +0900

    [Add]Polymorphism

--- polymorphism/Polymorphism01.java
+++ polymorphism/Polymorphism01.java
@@ -1,5 +1,15 @@
 package polymorphism;
 
-public class Polymorphism01 {
+import polymorphism.human.Employee;
+import polymorphism.human.Human;
+import polymorphism.human.Student;
 
+public class Polymorphism01 {
+	public static void main(String[] args) {
+		Human humna1 = new Student("佐藤", 17, 70);
+		System.out.println("Humanクラスのメソッド；プロフィールを紹介します。" + humna1.getProfile());
+
+		Human humna2 = new Employee("田中", 28, "システム部");
+		System.out.println("Humanクラスのメソッド；プロフィールを紹介します。" + humna2.getProfile());
+	}
 }
